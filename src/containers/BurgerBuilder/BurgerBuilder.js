6727109da1d0aa0a9dddbc1bf8d641b77cbd5c23
Fernando V <karate6789@gmail.com>
2019-04-19 21:05:06 -0400

    disabling the less button whenever there are 0 ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -48,29 +48,40 @@ addIngredientHandler =(type)=>{
 
 removeIngredientHandler =(type)=>{
     const oldCount = this.state.ingredients[type];
-    const updatedCount = oldCount -1;
-    const updatedIngredient = {
-        ...this.state.ingredients 
+    
+    if (oldCount!==0) {
+        const updatedCount = oldCount -1;    
+        const updatedIngredient = {
+            ...this.state.ingredients 
+        }
+        updatedIngredient[type]=updatedCount;  
+    
+        const priceDeduction = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice - priceDeduction; 
+        this.setState({
+            totalPrice:newPrice,
+            ingredients:updatedIngredient
+        })
     }
-    updatedIngredient[type]=updatedCount;  
-
-    const priceDeduction = INGREDIENT_PRICES[type];
-    const oldPrice = this.state.totalPrice;
-    const newPrice = oldPrice - priceDeduction; 
-    this.setState({
-        totalPrice:newPrice,
-        ingredients:updatedIngredient
-    })
+    
 }
 
 
     render(){
+        const disabledInfo={
+            ...this.state.ingredients
+        };
+        for(let key in disabledInfo){
+            disabledInfo[key]=disabledInfo[key] <=0 // {salad:true, meat:false}   //magic 
+        }
         return(   
             <Auxiliary>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls 
                     ingredientAdded={this.addIngredientHandler}
                     ingredientRemoved={this.removeIngredientHandler}
+                    disabled={disabledInfo}
                     />
             </Auxiliary>
         );
